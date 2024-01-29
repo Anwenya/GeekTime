91d46ffe365a4ef075e698404b75e65b4796e6ee
wuliuqi <1737682009@qq.com>
2024-01-29 20:14:17 +0800

    feat:封装token相关操作

---
+++ webook/token/jwt_maker_test.go
@@ -0,0 +1,68 @@
+package token
+
+import (
+	"github.com/Anwenya/GeekTime/webook/util"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+	uid := util.RandomInt(10, 100)
+	username := util.RandomString(10)
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, payload, err := maker.CreateToken(uid, username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	require.NotZero(t, payload.Tid)
+	require.Equal(t, username, payload.Username)
+	require.Equal(t, uid, payload.Uid)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt, time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToken(util.RandomInt(10, 20), util.RandomString(10), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomInt(10, 20), util.RandomString(10), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
---
+++ webook/token/payload.go
@@ -0,0 +1,51 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload
+type Payload struct {
+	jwt.RegisteredClaims
+	Tid       uuid.UUID `json:"tid"`
+	Uid       int64     `json:"uid"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issue_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+// NewPayload
+func NewPayload(uid int64, username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	now := time.Now()
+	payload := &Payload{
+		Tid:       tokenID,
+		Uid:       uid,
+		Username:  username,
+		IssuedAt:  now,
+		ExpiresAt: now.Add(duration),
+	}
+	return payload, nil
+}
+
+// Validate
+// 自定义校验函数 为了配合paseto
+// 如果不使用jwt.RegisteredClaims中的过期时间就需要自己额外做判断
+func (payload *Payload) Validate() error {
+	if time.Now().After(payload.ExpiresAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
