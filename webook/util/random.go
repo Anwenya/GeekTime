6501a7095dbaad7dfdf1135b9aa40a9b5119b817
wuliuqi <1737682009@qq.com>
2024-01-30 17:51:08 +0800

    feat:随机UA

--- webook/util/random.go
+++ webook/util/random.go
@@ -9,6 +9,15 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+var uaArray = [...]string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
+}
+
 func init() {
 	rand.NewSource(time.Now().UnixNano())
 }
@@ -30,4 +39,9 @@ func RandomString(n int) string {
 
 func RandomEmail() string {
 	return fmt.Sprintf("%s@email.com", RandomString(6))
+}
+
+func RandomUA() string {
+	k := len(uaArray)
+	return uaArray[rand.Intn(k)]
 }
