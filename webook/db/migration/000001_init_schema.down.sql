eeba6ad9bf0fcb01dd0fa8af3fdefd423ec6affe
wuliuqi <1737682009@qq.com>
2024-02-29 18:13:53 +0800

    db:更新数据库迁移文件

--- webook/db/migration/000001_init_schema.down.sql
+++ webook/db/migration/000001_init_schema.down.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS published_articles;
--- webook/db/migration/000001_init_schema.up.sql
+++ webook/db/migration/000001_init_schema.up.sql
@@ -1,13 +1,42 @@
-CREATE TABLE `users` (
-  `id` bigint PRIMARY KEY AUTO_INCREMENT,
-  `email` varchar(255) UNIQUE,
-  `password` varchar(255),
-  `nickname` varchar(128),
-  `birthday` bigint,
-  `bio` varchar(4096),
-  `phone` varchar(16) UNIQUE,
-  `wechat_open_id` varchar(32) UNIQUE,
-  `wechat_union_id` varchar(32),
-  `create_time` bigint,
-  `update_time` bigint
+CREATE TABLE `users`
+(
+    `id`              bigint PRIMARY KEY AUTO_INCREMENT,
+    `email`           varchar(255) UNIQUE,
+    `password`        varchar(255),
+    `nickname`        varchar(128),
+    `birthday`        bigint,
+    `bio`             longtext,
+    `phone`           varchar(16) UNIQUE,
+    `wechat_open_id`  varchar(32) UNIQUE,
+    `wechat_union_id` varchar(32),
+    `create_time`     bigint,
+    `update_time`     bigint
+);
+
+
+CREATE TABLE `articles`
+(
+    `id`          bigint AUTO_INCREMENT,
+    `title`       longtext,
+    `content`     longtext,
+    `author_id`   bigint,
+    `status`      tinyint unsigned,
+    `create_time` bigint,
+    `update_time` bigint,
+    PRIMARY KEY (`id`),
+    INDEX `idx_articles_author_id` (`author_id`)
+);
+
+
+CREATE TABLE `published_articles`
+(
+    `id`          bigint AUTO_INCREMENT,
+    `title`       longtext,
+    `content`     longtext,
+    `author_id`   bigint,
+    `status`      tinyint unsigned,
+    `create_time` bigint,
+    `update_time` bigint,
+    PRIMARY KEY (`id`),
+    INDEX `idx_published_articles_author_id` (`author_id`)
 );
